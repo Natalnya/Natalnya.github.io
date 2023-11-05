9e78fd6c75e0656d591c8089cca9eb1f4cc197c4
Natalnya <49555008+Natalnya@users.noreply.github.com>
2023-11-05 21:08:42 +0300

    Add files via upload

---
+++ lab1/Fly.java
@@ -0,0 +1,8 @@
+package lab1;
+
+public class Fly implements Move{
+    @Override
+    public void move(){
+        System.out.println(" is flying");
+    }
+}
---
+++ lab1/Ride.java
@@ -0,0 +1,8 @@
+package lab1;
+
+public class Ride implements Move{
+    @Override
+    public void move() {
+        System.out.println(" is riding by car");
+    }
+}
